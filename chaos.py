e472494b6187722d2886dae294c580407a0f11fb
Kevin Kielholz <kevinkielholz@web.de>
2017-05-23 17:22:16 +0200

    redirecting cerr and therefore logger to file

--- chaos.py
+++ chaos.py
@@ -47,61 +47,63 @@ def install_requirements():
 
 if __name__ == "__main__":
     with open("cout.txt", "a") as cout:
-        with contextlib.redirect_stdout(cout)
-            logging.info("starting up and entering event loop")
-            
-            os.system("pkill chaos_server")
-            subprocess.Popen([sys.executable, "server.py"], cwd=join(THIS_DIR, "server"))
-            
-            while True:
-                log.info("looking for PRs")
+        with contextlib.redirect_stdout(cout):
+            with open("cerr.txt", "a") as cerr:
+                with contextlib.redirect_stderr(cerr):
+                    logging.info("starting up and entering event loop")
+                    
+                    os.system("pkill chaos_server")
+                    subprocess.Popen([sys.executable, "server.py"], cwd=join(THIS_DIR, "server"))
+                    
+                    while True:
+                        log.info("looking for PRs")
 
-                now = arrow.utcnow()
-                voting_window = gh.voting.get_voting_window(now)
-                prs = gh.prs.get_ready_prs(api, settings.URN, voting_window)
+                        now = arrow.utcnow()
+                        voting_window = gh.voting.get_voting_window(now)
+                        prs = gh.prs.get_ready_prs(api, settings.URN, voting_window)
 
-                needs_update = False
-                for pr in prs:
-                    pr_num = pr["number"]
-                    logging.info("processing PR #%d", pr_num)
+                        needs_update = False
+                        for pr in prs:
+                            pr_num = pr["number"]
+                            logging.info("processing PR #%d", pr_num)
 
-                    votes = gh.voting.get_votes(api, settings.URN, pr)
-                
-                    # is our PR approved or rejected?
-                    vote_total = gh.voting.get_vote_sum(api, votes)
-                    threshold = gh.voting.get_approval_threshold(api, settings.URN)
-                    is_approved = vote_total >= threshold
+                            votes = gh.voting.get_votes(api, settings.URN, pr)
+                        
+                            # is our PR approved or rejected?
+                            vote_total = gh.voting.get_vote_sum(api, votes)
+                            threshold = gh.voting.get_approval_threshold(api, settings.URN)
+                            is_approved = vote_total >= threshold
 
-                    if is_approved:
-                        log.info("PR %d approved for merging!", pr_num)
-                        try:
-                            gh.prs.merge_pr(api, settings.URN, pr, votes, vote_total,
-                                    threshold)
-                        # some error, like suddenly there's a merge conflict, or some
-                        # new commits were introduced between findint this ready pr and
-                        # merging it
-                        except gh_exc.CouldntMerge:
-                            log.info("couldn't merge PR %d for some reason, skipping",
-                                    pr_num)
-                            gh.prs.label_pr(api, settings.URN, pr_num, ["can't merge"])
-                            continue
+                            if is_approved:
+                                log.info("PR %d approved for merging!", pr_num)
+                                try:
+                                    gh.prs.merge_pr(api, settings.URN, pr, votes, vote_total,
+                                            threshold)
+                                # some error, like suddenly there's a merge conflict, or some
+                                # new commits were introduced between findint this ready pr and
+                                # merging it
+                                except gh_exc.CouldntMerge:
+                                    log.info("couldn't merge PR %d for some reason, skipping",
+                                            pr_num)
+                                    gh.prs.label_pr(api, settings.URN, pr_num, ["can't merge"])
+                                    continue
 
-                        gh.prs.label_pr(api, settings.URN, pr_num, ["accepted"])
-                        needs_update = True
+                                gh.prs.label_pr(api, settings.URN, pr_num, ["accepted"])
+                                needs_update = True
 
-                    else:
-                        log.info("PR %d rejected, closing", pr_num)
-                        gh.comments.leave_reject_comment(api, settings.URN, pr_num)
-                        gh.prs.label_pr(api, settings.URN, pr_num, ["rejected"])
-                        gh.prs.close_pr(api, settings.URN, pr)
+                            else:
+                                log.info("PR %d rejected, closing", pr_num)
+                                gh.comments.leave_reject_comment(api, settings.URN, pr_num)
+                                gh.prs.label_pr(api, settings.URN, pr_num, ["rejected"])
+                                gh.prs.close_pr(api, settings.URN, pr)
 
 
-                # we approved a PR, restart
-                if needs_update:
-                    logging.info("updating code and requirements and restarting self")
-                    update_self_code()
-                    install_requirements()
-                    restart_self()
+                        # we approved a PR, restart
+                        if needs_update:
+                            logging.info("updating code and requirements and restarting self")
+                            update_self_code()
+                            install_requirements()
+                            restart_self()
 
-                logging.info("sleeping for %d seconds", settings.SLEEP_TIME)
-                time.sleep(settings.SLEEP_TIME)
+                        logging.info("sleeping for %d seconds", settings.SLEEP_TIME)
+                        time.sleep(settings.SLEEP_TIME)
